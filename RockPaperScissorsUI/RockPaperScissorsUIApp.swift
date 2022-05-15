237c94bd61b7af7eb496c69c870feba9203e4fb9
daukesnurmukhan <daukesnurmukhan@gmail.com>
2022-05-16 00:18:56 +0600

    Initial Commit

---
+++ RockPaperScissorsUI/RockPaperScissorsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsUIApp.swift
+//  RockPaperScissorsUI
+//
+//  Created by Нұрмұхан Дәукес on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
